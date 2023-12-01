6f7c8d8f0308843b751dce2c3c3f21c353d4a6f4
ndnghia2410 <91201271+ndnghia24@users.noreply.github.com>
2023-12-01 13:39:15 +0700

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,6 +46,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.2.1")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
 
--- app/src/main/java/com/example/moviebooking/ui/app/allmovies/AllMovieActivity.java
+++ app/src/main/java/com/example/moviebooking/ui/app/allmovies/AllMovieActivity.java
@@ -1,7 +1,9 @@
 package com.example.moviebooking.ui.app.allmovies;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,21 +12,50 @@ import com.example.moviebooking.R;
 import com.example.moviebooking.data.HardcodingData;
 import com.example.moviebooking.data.SharedReferenceController;
 import com.example.moviebooking.dto.Movie;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class AllMovieActivity extends AppCompatActivity {
+    DatabaseReference moviesReference;
     List<Movie> allMovieList = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_movies);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        moviesReference = database.getReference("MOVIES");
+        moviesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                allMovieList.clear();
+
+                for (DataSnapshot movieSnapshot : snapshot.getChildren()) {
+                    Movie movie = movieSnapshot.getValue(Movie.class);
+                    if (movie != null) {
+                        allMovieList.add(movie);
+                        Log.d("MovieInfo", "Title: " + movie.getTitle());
+                    } else {
+                        Log.d("MovieInfo", "Movie is null");
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("TAG", "Lỗi: " + error.getMessage());
+            }
+        });
+
         setDataForMoviesSlider();
     }
 
     private void setDataForMoviesSlider() {
-        allMovieList = SharedReferenceController.getListMovies(this);
+        //allMovieList = SharedReferenceController.getListMovies(this);
 
         // convert to grid
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
